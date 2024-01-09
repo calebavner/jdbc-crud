4e17e941ed7050f24c6c0f1ec6c53f278bac3ffa
calebavner <avner.trabalho@hotmail.com>
2024-01-09 11:55:00 -0300

    Criação do SellerDaoJDBC e do DaoFactory

---
+++ JDBC-crud/src/main/java/org/midasvision/model/dao/DaoFactory.java
@@ -0,0 +1,8 @@
+package org.midasvision.model.dao;
+
+public class DaoFactory {
+
+    public static SellerDao createSellerDao() {
+        return new SellerDaoJDBC();
+    }
+}
---
+++ JDBC-crud/src/main/java/org/midasvision/model/dao/SellerDaoJDBC.java
@@ -0,0 +1,33 @@
+package org.midasvision.model.dao;
+
+import org.midasvision.model.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao{
+
+    @Override
+    public void insert(Seller s) {
+
+    }
+
+    @Override
+    public void update(Seller s) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
