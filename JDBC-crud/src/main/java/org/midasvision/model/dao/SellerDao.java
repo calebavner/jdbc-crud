e780267cb57229a437d1979f260c5aedc2e30b6b
calebavner <avner.trabalho@hotmail.com>
2024-01-09 11:48:37 -0300

    Criação das interfaces DAO de Seller e Department

---
+++ JDBC-crud/src/main/java/org/midasvision/model/dao/SellerDao.java
@@ -0,0 +1,19 @@
+package org.midasvision.model.dao;
+
+import org.midasvision.model.Department;
+import org.midasvision.model.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+
+    void insert(Seller s);
+
+    void update(Seller s);
+
+    void deleteById(Integer id);
+
+    Seller findById(Integer id);
+
+    List<Seller> findAll();
+}
