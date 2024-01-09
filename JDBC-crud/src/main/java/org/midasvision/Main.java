ac6750a70537d0ff6136b06b54750260332e073b
calebavner <avner.trabalho@hotmail.com>
2024-01-08 22:16:42 -0300

    Criação da classe Department

--- JDBC-crud/src/main/java/org/midasvision/Main.java
+++ JDBC-crud/src/main/java/org/midasvision/Main.java
@@ -1,7 +1,8 @@
 package org.midasvision;
 
+import org.midasvision.model.Department;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
